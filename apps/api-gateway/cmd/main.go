969193537a7b481f136941136018f0ec8aaf39a7
Noam Kesten <noamkesten@fb.com>
2025-02-18 11:59:09 +0200

    added grpc request and response

--- apps/api-gateway/cmd/main.go
+++ apps/api-gateway/cmd/main.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"api-gateway/pkg/routers"
-	"context"
-	"log"
-	"time"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewWeddingServiceClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
 
 	r := routers.NewRouter()
 	r.Run("localhost:8080")
